8436e1e40fd01d67eb4bd084c581fa75023607d1
Prabhat <prabhat328@outlook.com>
2024-12-14 15:52:38 +0530

    included user id in login response

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -18,7 +18,7 @@ export const login = async (req, res) => {
             return res.status(401).json({ message: "Invalid password" });
         }
 
-        return res.status(200).json({ message: "Login successful" });
+        return res.status(200).json({ message: "Login successful", userId: user._id });
     } catch (error) {
         console.error("Error during login:", error);
         return res.status(500).json({ message: "Internal server error" });
